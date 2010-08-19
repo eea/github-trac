5a4e3faed756605bff60d1917a2282172e347b7a
Christoph Otto <christoph_git@mksig.org>
2010-08-19 01:06:54 -0700

    add some initial untested installer code and detect svn changeset requests

--- github/github.py
+++ github/github.py
@@ -1,22 +1,32 @@
 from trac.core import *
 from trac.config import Option, IntOption, ListOption, BoolOption
 from trac.web.api import IRequestFilter, IRequestHandler, Href
+from trac.env import IEnvironmentSetupParticipant
 from trac.util.translation import _
 from hook import CommitHook
 
 import simplejson
+import re
 
 from git import Git
 
 class GithubPlugin(Component):
-    implements(IRequestHandler, IRequestFilter)
-    
-    
-    key = Option('github', 'apitoken', '', doc="""Your GitHub API Token found here: https://github.com/account, """)
-    closestatus = Option('github', 'closestatus', '', doc="""This is the status used to close a ticket. It defaults to closed.""")
-    browser = Option('github', 'browser', '', doc="""Place your GitHub Source Browser URL here to have the /browser entry point redirect to GitHub.""")
-    autofetch = Option('github', 'autofetch', '', doc="""Should we auto fetch the repo when we get a commit hook from GitHub.""")
-    repo = Option('trac', 'repository_dir' '', doc="""This is your repository dir""")
+    implements(IRequestHandler, IRequestFilter, IEnvironmentSetupParticipant)
+
+
+    key           = Option('github', 'apitoken',      '', doc = """Your GitHub API Token found here: https://github.com/account, """)
+    closestatus   = Option('github', 'closestatus',   '', doc = """This is the status used to close a ticket. It defaults to closed.""")
+    browser       = Option('github', 'browser',       '', doc = """Place your GitHub Source Browser URL here to have the /browser entry point redirect to GitHub.""")
+    autofetch     = Option('github', 'autofetch',     '', doc = """Should we auto fetch the repo when we get a commit hook from GitHub.""")
+    repo          = Option('trac',   'repository_dir' '', doc = """This is your repository dir""")
+    revmap        = Option('github', 'svn_revmap',    '', doc = """a plaintext file mapping svn revisions to git hashes""")
+    reread_revmap = Option('github', 'reread_revmap',  0, doc = """force the rereading of the revmap""")
+
+    SCHEMA = Table('svn_revmap', key = ('svn_rev'))[
+            Column('svn_rev'),
+            Column('git_hash'),
+            Index(['svn_rev', 'git_hash']),]
+
 
     def __init__(self):
         self.hook = CommitHook(self.env)
@@ -24,7 +34,60 @@ class GithubPlugin(Component):
         self.env.log.debug("Browser: %s" % self.browser)
         self.processHook = False
 
-    
+    # IEnvironmentSetupParticpant methods
+    def environment_created(self):
+        if revmap:
+            self._upgrade_db(self.env.get_db_cnx())
+
+    #return true if the db table doesn't exist or needs to be updated
+    def environment_needs_upgrade(self, db):
+        if not revmap:
+            return False
+        if reread_revmap:
+            self.config.set('github.reread_revmap', 0)
+            self.config.save()
+            return True
+        cursor = db.cursor()
+        try:
+            cursor.execute("SELECT COUNT(*) FROM svn_revmap")
+            row = cursor.fetchone()
+            #if there's one or more rows, assume everything's ok
+            if row[0] > 0:
+                return False
+            return True
+        except:
+            return True
+
+    def upgrade_environment(self, db):
+        self._upgrade_db(db)
+
+    def _upgrade_db(self, db):
+        #open the revision map
+        try:
+            revmap_fd = open(revmap, 'rb')
+        except IOError:
+            raise ResourceNotFound(_("revision map '%s' not found", revmap)
+        cursor = db.cursor()
+        try:
+            cursor.execute("DROP TABLE svn_revmap;")
+        except:
+            pass
+
+        db_backend = db.get_db_cnx()
+        for stmt in db_backend.to_sql(self.SCHEMA):
+            self.env.log(stmt)
+            cursor.execute(stmt)
+
+        insert_count = 0
+        for line in revmap_fd:
+            [svn_rev, git_hash] = line.split()
+            insert_query = "INSERT INTO svn_revmap (svn_rev, git_hash) VALUES (%s, %s)" % (svn_rev, git_hash)
+            self.env.log.debug(insert_query)
+            cursor.execute(insert_query)
+            insert_count++
+
+        self.env.log.debug("inserted %d mappings into svn_revmap" % insert_count)
+
     # IRequestHandler methods
     def match_request(self, req):
         self.env.log.debug("Match Request")
@@ -37,7 +100,7 @@ class GithubPlugin(Component):
 
         self.env.log.debug("Handle Request: %s" % serve)
         return serve
-    
+
     def process_request(self, req):
         if self.processHook:
             self.processCommitHook(req)
@@ -66,8 +129,16 @@ class GithubPlugin(Component):
     def processChangesetURL(self, req):
         self.env.log.debug("processChangesetURL")
         browser = self.browser.replace('/tree/master', '/commit/')
-        
+
         url = req.path_info.replace('/changeset/', '')
+        self.env.log.debug("url is %s" % url)
+        svn_rev_match = re.match( '^([0-9]{1,6})([^0-9a-fA-F]|$)', url)
+        if svn_rev_match:
+            svn_rev = svn_rev_match.group(1)
+            self.env.log.debug("found a svn revision: %s" % svn_rev);
+        else:
+            self.env.log.debug("didn't find a svn revision");
+
         if not url:
             browser = self.browser
             url = ''
@@ -83,7 +154,7 @@ class GithubPlugin(Component):
         self.env.log.debug("processBrowserURL")
         browser = self.browser.replace('/master', '/')
         rev = req.args.get('rev')
-        
+
         url = req.path_info.replace('/browser', '')
         if not rev:
             rev = ''
@@ -94,7 +165,7 @@ class GithubPlugin(Component):
 
         req.redirect(redirect)
 
-        
+
 
     def processCommitHook(self, req):
         self.env.log.debug("processCommitHook")
@@ -103,7 +174,7 @@ class GithubPlugin(Component):
             status = 'closed'
 
         data = req.args.get('payload')
-         
+
         if data:
             jsondata = simplejson.loads(data)
 
